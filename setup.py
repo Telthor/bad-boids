c4f29370ec2966bde768b40d65f5cf6816c85637
David Wise <wisedavid@mac.com>
2016-02-25 21:58:34 +0000

    fiddled with setup.py file - changed command file from boids.py to boids

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-        name = 'Boids'
-        version = '0.0.1'
-        packages = find_packages()
-        scripts = ['boids/boids.py']
-        install_requires = ['matplotlib','numpy','nose','yaml','random']
-        license = 'MIT'
-        author = 'David Wise'
-        author_email = 'd.wise.15@ucl.ac.uk'
+        name = 'Boids',
+        version = '0.0.1',
+        packages = find_packages(),
+        install_requires = ['matplotlib','numpy','nose'],
+        license = 'MIT',
+        author = 'David Wise',
+        author_email = 'd.wise.15@ucl.ac.uk',
         scripts = ['scripts/boids']
 )
