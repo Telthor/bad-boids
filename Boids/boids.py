2fffddfe95ae77cccf8d7c136f8c2ca5c4c5d482
David Wise <wisedavid@mac.com>
2016-02-29 12:42:47 +0000

    Minor change to boids.py header

--- Boids/boids.py
+++ Boids/boids.py
@@ -1,5 +1,5 @@
 """
-A deliberately bad implementation of [Boids](http://dl.acm.org/citation.cfm?doid=37401.37406)
+A marginally improved implementation of [Boids](http://dl.acm.org/citation.cfm?doid=37401.37406)
 for use as an exercise on refactoring.
 """
 
